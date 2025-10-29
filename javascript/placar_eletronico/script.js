647a528fcdfb6a7516737c3fb6cf2071f58ecc2a
Ricardo Dias <ricardo.dias@hotmail.com.br>
2025-10-29 13:03:57 -0300

    Grid

---
+++ javascript/placar_eletronico/script.js
@@ -0,0 +1,54 @@
+function ponto1(){
+    var placar = document.querySelector("#pt1");
+    var pt1 = Number(placar.innerHTML);
+    // pt1 = pt1 + 1;
+    pt1++;
+    // pt1 += 1;
+    placar.innerHTML = pt1;
+}
+function ponto3(){
+    var placar = document.querySelector("#pt1");
+    var pt1 = Number(placar.innerHTML);
+    if(pt1 > 0){
+        pt1--;
+        placar.innerHTML = pt1;
+    }
+}
+function ponto2(){
+    var placar = document.querySelector("#pt2");
+    var pt2 = Number(placar.innerHTML);
+    // pt1 = pt1 + 1;
+    pt2++;
+    // pt1 += 1;
+    placar.innerHTML = pt2;
+}
+function ponto4(){
+    var placar = document.querySelector('#pt2');
+    var pt2 = Number(placar.innerHTML);
+
+    if(pt2 > 0){
+        pt2--;
+        placar.innerHTML = pt2;
+    }
+}
+
+function zerar(){
+    var placar1 = document.querySelector("#pt1");
+    var placar2 = document.querySelector("#pt2");
+    placar1.innerHTML = 0;
+    placar2.innerHTML = 0;
+}
+
+
+function pontobasquete1(ponto){
+    var placar = document.querySelector('#pt3');
+    var pontoNaTela = Number(placar.innerHTML);
+    pontoNaTela = pontoNaTela + ponto;
+    placar.innerHTML = pontoNaTela;
+}
+function pontobasquete2(ponto){
+    var placar = document.querySelector('#pt4');
+    var pontoNaTela = Number(placar.innerHTML);
+    pontoNaTela = pontoNaTela + ponto;
+    placar.innerHTML = pontoNaTela;
+}
